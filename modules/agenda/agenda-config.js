19e63b6be4dd50bd00390b5ee894a590d1199d07
Adrian Alberto Mora Sinning <43191656+AdrianAlbertoMoraSinning@users.noreply.github.com>
2026-05-31 11:25:57 -0600

    Update agenda-config.js

--- modules/agenda/agenda-config.js
+++ modules/agenda/agenda-config.js
@@ -7,7 +7,7 @@ window.AGENDA_CONFIG = {
   ownerEmail: "abogadosasociadosap4@mail.com",
   ownerWhatsapp: "573108030751",
   adminPin: "3108030751",
-  appsScriptUrl: "PASTE_GOOGLE_APPS_SCRIPT_WEB_APP_URL_HERE",
+  appsScriptUrl: "https://script.google.com/macros/s/AKfycbwjxYqU1jBtczlPz7-ONbvfOdPr25BT4vcYq3T8E9T9fyMLYw3axrmY0N1AFQk0srVF/exec",
   workDays: [1, 2, 3, 4, 5],
   startHour: 8,
   endHour: 17,
